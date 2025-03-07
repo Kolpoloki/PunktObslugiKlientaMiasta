214e9c4f171b4de5dea44334d8498b95c299ac29
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:12:42 +0100

    enum1

--- src/zadanie/RodzajDokumentu.java
+++ src/zadanie/RodzajDokumentu.java
@@ -1,5 +1,23 @@
 package zadanie;
 
 public enum RodzajDokumentu {
+    PASZPORT("Paszport"),
+    DOWOD_OSOBISTY("Dowód Osobisty"),
+    PRAWO_JAZDY("Prawo Jazdy"),
+    DOWOD_REJESTRACYJNY("Dowód Osobisty"),
+    LEGITYMACJA_SZKOLNA("Legitymacja Szkolna"),
+    LEGITYMACJA_STUDENCKA("Legitymacja Studencka");
+
+    private final String opis;
+
+    RodzajDokumentu(String opis) {this.opis=opis;}
+
+    public String getOpis() {return opis;}
+
+    public static boolean czyPoprawny(String opis){
+        for(RodzajDokumentu dokument : RodzajDokumentu.values())
+            if (dokument.getOpis().equalsIgnoreCase(opis)) return true;
+        return false;
+    }
 
 }
