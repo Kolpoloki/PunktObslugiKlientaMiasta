306e1bb1da62277f4b5d54ba796da116431606c0
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:13:28 +0100

    enum2

--- src/zadanie/RodzajPlatnosci.java
+++ src/zadanie/RodzajPlatnosci.java
@@ -1,5 +1,19 @@
 package zadanie;
 
 public enum RodzajPlatnosci {
+    KARTA("Karta"),
+    GOTOWKA("Gotówka"),
+    BLIK("Blik");
 
+    private final String opis;
+
+    RodzajPlatnosci(String opis) {this.opis=opis;}
+
+    public String getOpis() {return opis;}
+
+    public static boolean czyPoprawny(String opis){
+        for(RodzajPlatnosci rodzajPlatnosci : RodzajPlatnosci.values())
+            if (rodzajPlatnosci.getOpis().equalsIgnoreCase(opis)) return true;
+        return false;
+    }
 }
