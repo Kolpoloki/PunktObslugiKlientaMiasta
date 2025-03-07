555d0629e10a343906549b7c7440adbb9f378f4a
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:14:06 +0100

    enum3

--- src/zadanie/Status.java
+++ src/zadanie/Status.java
@@ -1,5 +1,19 @@
 package zadanie;
 
 public enum Status {
+    ZLOZONA("Żłożona"),
+    W_TRAKCIE("W Trakcie"),
+    ZREALIZOANA("Zrealizowana");
 
+    private final String opis;
+
+    Status(String opis) {this.opis=opis;}
+
+    public String getOpis() {return opis;}
+
+    public static boolean czyPoprawny(String opis){
+        for(Status status : Status.values())
+            if (status.getOpis().equalsIgnoreCase(opis)) return true;
+        return false;
+    }
 }
