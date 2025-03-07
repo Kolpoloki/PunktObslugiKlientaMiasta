43654a33c50c4529d5b833a3d7619dad34ed9f9e
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:16:08 +0100

    class2

--- src/zadanie/Sprawa.java
+++ src/zadanie/Sprawa.java
@@ -1,6 +1,51 @@
 package zadanie;
 
+import java.time.LocalDate;
 
 public class Sprawa {
+    private Obywatel obywatel;
+    private String opis;
+    private LocalDate dataZlozenia;
+    private Status status;
+    private Platnosc platnosc;
+    private String dzial;
 
+    public Sprawa(Obywatel obywatel, String opis, LocalDate dataZlozenia, Status status, Platnosc platnosc) {
+        this.obywatel = obywatel;
+        this.opis = opis;
+        this.dataZlozenia = dataZlozenia;
+        this.status = status;
+        this.platnosc = platnosc;
+        this.dzial="nie przypisany";
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public LocalDate getDataZlozenia() {
+        return dataZlozenia;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Platnosc getPlatnosc() {
+        return platnosc;
+    }
+
+    public void setDzial(String dzial) {this.dzial = dzial;}
+
+    @Override
+    public String toString() {
+        return "Sprawa{" +
+                "obywatel=" + obywatel +
+                ", opis='" + opis + '\'' +
+                ", dataZlozenia=" + dataZlozenia +
+                ", status=" + status +
+                ", platnosc=" + platnosc +
+                ", dzial='" + dzial + '\'' +
+                '}';
+    }
 }
