906b4bc60f45661ee0f0e8f08f5480c33940e48b
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:16:48 +0100

    class3

--- src/zadanie/Platnosc.java
+++ src/zadanie/Platnosc.java
@@ -1,5 +1,34 @@
 package zadanie;
 
-public class Platnosc  {
+public class Platnosc implements OperacjePlatnosci {
+    private double ilosc;
+    private RodzajPlatnosci rodzajPlatnosci;
+    private Status status;
 
+    public Platnosc(double ilosc, RodzajPlatnosci rodzajPlatnosci,Status status) {
+        this.ilosc = ilosc;
+        this.rodzajPlatnosci = rodzajPlatnosci;
+        this.status = status;
+    }
+
+    public double sprawdzIlosc() {
+        return ilosc;
+    }
+
+    public RodzajPlatnosci sprawdzRodzajPlatnosci() {
+        return rodzajPlatnosci;
+    }
+    public Status sprawdzStatusPlatnosci() {return status;}
+    public void wykonajPlatnosc(){
+        System.out.println("Płatność została dokonana.");
+    };
+
+    @Override
+    public String toString() {
+        return "Platnosc{" +
+                "ilosc=" + ilosc +
+                ", rodzajPlatnosci=" + rodzajPlatnosci +
+                ", status=" + status +
+                '}';
+    }
 }
