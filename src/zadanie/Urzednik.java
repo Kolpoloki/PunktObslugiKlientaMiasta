7ad58311ac51e0c35996e006291099f71b7d56ad
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:19:39 +0100

    class5

--- src/zadanie/Urzednik.java
+++ src/zadanie/Urzednik.java
@@ -1,5 +1,27 @@
 package zadanie;
 
-public class Urzednik {
+public class Urzednik extends Obywatel{
+    private String stanowisko;
 
+    public Urzednik(String imie, String nazwisko, String adres, String telefon, String stanowisko) {
+        super(imie, nazwisko, adres, telefon);
+        this.stanowisko = stanowisko;
+    }
+    public Urzednik(String imie, String nazwisko, String telefon, String stanowisko) {
+        super(imie, nazwisko, telefon);
+        this.stanowisko = stanowisko;
+    }
+    public Urzednik(String imie, String nazwisko, String stanowisko) {
+        super(imie, nazwisko);
+        this.stanowisko = stanowisko;
+    }
+
+    public String getStanowisko() {
+        return stanowisko;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"stanosisko="+stanowisko;
+    }
 }
