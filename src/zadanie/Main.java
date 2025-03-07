2c6647cee4bc935a0d74c09edddb21ea05d894aa
Kolpoloki <patryk2008nyy@gmail.com>
2025-03-07 21:08:50 +0100

    structure

---
+++ src/zadanie/Main.java
@@ -0,0 +1,14 @@
+package zadanie;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/zadanie/Sprawa.java
@@ -0,0 +1,6 @@
+package zadanie;
+
+
+public class Sprawa {
+
+}
